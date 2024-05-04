d0270dfcf2ec2cb231abb12d0f4160ccbb2a3b66
Ivan Slovinsky <slovanya1@gmail.com>
2024-05-04 18:25:54 +0300

    Eighth step (added brain-progression)

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Brain-Even. First game example.
 [![asciicast](https://asciinema.org/a/043AcrpfqaLdILrnsEGExckuv.svg)](https://asciinema.org/a/043AcrpfqaLdILrnsEGExckuv)
 [Brain-Calc.](https://asciinema.org/a/6Um8kSgk04F6EwlzC8Ou4h4SL)
 [Brain-Gcd.](https://asciinema.org/a/IRI9P7twDzrmZkm0XyAaI58m1)
+[Brain-Progression.](https://asciinema.org/a/YaF66wmyHfaZl0JXaEKn3aydM)
 
 <a href="https://codeclimate.com/github/slovanya/php-project-45/maintainability"><img src="https://api.codeclimate.com/v1/badges/f8cf69fc2a8b2ca156f4/maintainability" /></a>
 
---
+++ src/Games/brain-progression.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace BrainGames\Progression;
+
+use function cli\line;
+use function cli\prompt;
+
+function generateProgression(int $firstNumber, int $increment, int $length): array
+{
+    $row = [];
+    for ($i = $firstNumber; $i < $firstNumber + $increment * $length; $i += $increment) {
+        $row[] = $i;
+    }
+    return $row;
+}
+
+function getPokedProgression(array $row, int $place): string
+{
+    $row[$place] = '..';
+    return implode(' ', $row);
+}
+
+function start_Progression(int $rounds): bool
+{
+    line('What number is missing in the progression?');
+    for ($i = 0; $i < $rounds; $i++) {
+        $firstNumber = rand(1, 100);
+        $length = rand(5, 10);
+        $increment = rand(1, 5);
+        echo "$firstNumber $increment $length";
+        $row = generateProgression($firstNumber, $increment, $length);
+        $place = rand(0, $length);
+        $pokedNumber = $firstNumber + $increment * $place;
+        $res = (string) $pokedNumber;
+        $pokedRow = getPokedProgression($row, $place);
+        line("Question: {$pokedRow}");
+        $answer = prompt("Your answer");
+        if ($res === $answer) {
+            line('Correct!');
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $res);
+            return false;
+        }
+    }
+    return true;
+}
